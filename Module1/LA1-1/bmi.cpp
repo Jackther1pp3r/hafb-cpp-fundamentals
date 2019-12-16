d178f7b1e37e0ef7ba03ed63143b5da115910af2
jack.fernald@gmail.com <fk0CjFeCc0%2@G5Sr6$wB7bsP>
2019-12-16 11:32:48 -0700

    Mi

---
+++ Module1/LA1-1/bmi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // calculate BMI; wight(kg)/height(m)]^2
+    //floating point variables: float, double
+
+    float weight = 0, height = 0, bmi = 0;
+    
+    cout << "Please enter weight: " << endl;
+    cin >> weight;
+    cout << "Please enter height: " << endl;
+    cin >> height;
+
+    bmi = weight/(height*height);
+
+    cout << "Your BMI is: " << bmi << endl;
+
+
+    return 0;
+}
