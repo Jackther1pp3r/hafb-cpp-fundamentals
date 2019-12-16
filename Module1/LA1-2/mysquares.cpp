a9fb8ee895e0851ee065b649f72c26e6de0d953a
jack.fernald@gmail.com <fk0CjFeCc0%2@G5Sr6$wB7bsP>
2019-12-16 13:53:25 -0700

    Next commit

---
+++ Module1/LA1-2/mysquares.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file mysquares.cpp
+ * @TheJackFernald {Jack Fernald} ({jack@fernald.cc})
+ * @brief 
+ * @version 0.1
+ * @date 2019-12-16
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#include <iostream>
+#include <iomanip> //for formatted output
+#include <cmath> // For sqrt() function
+using namespace std;
+
+const int kMaxCount = 100;
+
+
+int main()
+{
+    cout << "NUMBER    SQUARE ROOT" << endl;
+    cout << "------    -----------" << endl;
+    cout << setiosflags(ios::showpoint);
+    for (int count = 1; count < kMaxCount; ++count)
+    {
+        cout << setw(4) << count 
+    }
+
+    cout << 81 << sqrt(81) << endl;
+    return 0;
+
+}
--- Notes.md
+++ Notes.md
@@ -1,1 +1,34 @@
-This is a file for notes:
+
+
+## This file is for taking notes during the class
+
+cppreference.com
+
+## Syntax
+- Single ticks 'a' are used for chars 
+- Quotes "hello" are used for strings 
+
+
+
+## Logic Structures
+- This is a  `switch` logic structure.
+```
+switch (code)
+{
+    case 'S':
+    case 's': //This acts as a this and that
+        cout << "blah blah" >> endl;
+        break;
+    case 'M':
+        cout <<"Hello">> endl;
+        break;
+}
+```
+
+- This is a `for` loop.
+```
+for (int count = 1; count < kMaxCount; ++count)
+{
+    code;
+}
+```
