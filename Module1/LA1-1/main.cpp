181b2a4f85d0e588210ac058f66ef1447d589b2e
jack.fernald@gmail.com <fk0CjFeCc0%2@G5Sr6$wB7bsP>
2019-12-16 10:22:08 -0700

    new

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 
-using hello
+using namespace std;
+
+int main()
+{
+    int age;
+
+    std:cout << "My name is Jack" << std::endl;
+    cout << "Hello Word" << endl;
+    cout << "what is your age?" <<  endl;
+    cin >> age;
+    cout << "I see you are: " << age << endl;
+    cout << "inetegers are: " << sizeof(int) << " bytes" << endl;
+
+    cout << "Min integers value: " << INT32_MIN <<
+        " Mas Integers value: " << INT32_MAX << "bytes" << endl;
+        
+    return 0;
+}
