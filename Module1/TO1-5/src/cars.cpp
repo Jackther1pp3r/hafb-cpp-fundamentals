a6202e11d7ff42c29a8b10252de02d100662af82
jack.fernald@gmail.com <fk0CjFeCc0%2@G5Sr6$wB7bsP>
2019-12-17 14:09:22 -0700

    namespace

--- Module1/TO1-5/src/cars.cpp
+++ Module1/TO1-5/src/cars.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
 #include "cars.h"
+
+int HorsePower()
+{
+    return 150;
+}
+
+void lamborghini::cout()
+{
+    std::cout << "supercar" << std::endl;
+}
+
+int lamborghini::HorsePower()
+{
+    return 760;
+}
+
+int porsche::HorsePower()
+{
+    return 700;
+}
 // using namespace lamborghini;
 // using namespace porsche;
--- Module1/TO1-5/src/cars.h
+++ Module1/TO1-5/src/cars.h
@@ -1,5 +1,17 @@
 #ifndef CARS_H_
 #define CARS_H_
 
+namespace lamborghini
+{
+    int HorsePower(); //inside Lamborghini namespace
+    void cout();
+}
+
+namespace porsche
+{
+    int HorsePower(); //inside porsche namespace
+}
+
+int HorsePower(); //"standard" namespace
 
 #endif /* !CARS_H_ */
