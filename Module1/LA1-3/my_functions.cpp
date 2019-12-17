dca347c178d8a74fbc5732f6d7cc7f2f2ccbb75d
jack.fernald@gmail.com <fk0CjFeCc0%2@G5Sr6$wB7bsP>
2019-12-17 10:53:29 -0700

    git ignore update, my_function update

---
+++ Module1/LA1-3/my_functions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "my_functions.h"
+using namespace std;
+
+
+
+//Function Definition
+int Sum(int num1, int num2)
+{
+  return num1 + num2;
+}
---
+++ Module1/LA1-3/my_functions.h
@@ -0,0 +1,6 @@
+#ifndef MY_FUNCTIONS_
+#define MY_FUNCTIONS_
+
+int Sum(int num1, int num2);
+
+#endif /* !FACTOR_H_ */
