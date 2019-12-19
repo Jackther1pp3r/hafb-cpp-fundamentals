4285319aececc8165d16787cfe4622c8c7d2a7d5
jack.fernald@gmail.com <fk0CjFeCc0%2@G5Sr6$wB7bsP>
2019-12-19 11:04:59 -0700

    hello

--- Module3/TO3-1/src/distance.cpp
+++ Module3/TO3-1/src/distance.cpp
@@ -1,7 +1,41 @@
-// countpp3.cpp
-// increment counter variable with ++ operator
-// uses unnamed temporary object
 #include <iostream>
-#include <cmath> 
-using namespace std;
 #include "distance.h"
+
+/**
+ * @brief Show the distance and inches in:
+ * feet'-inches" format.
+ * 
+ */
+void Distance::ShowDist() const
+{
+    std::cout << feet() << "\'-" << inches() << "\"" << std::endl; 
+}
+
+/**
+ * @brief Plus + Operator for Distance objects
+ * 
+ * @param d2 Second Distance object
+ * @return Distance : Sum of self + d2 objects
+ */
+Distance Distance::operator + (Distance rhs) const
+{
+    int feet = feet_ + rhs.feet_;
+    float inches = inches_ + rhs.inches_;
+    // Update values IF inches > 12
+    if(inches >= 12.0)
+    {
+        inches -= 12.0;
+        feet++;
+    }
+    // Distance temp(feet, inches);
+    // return temp;
+    return Distance(feet, inches);
+}
+
+
+ std::ostream &operator <<(std::ostream &os, const Distance& distance)
+ {
+     os << "Feet: " << distance.feet_ << " Inches: " << distance.inches_;
+     return os;
+
+ };
--- Module3/TO3-1/src/distance.h
+++ Module3/TO3-1/src/distance.h
@@ -1,1 +1,29 @@
-#include <iostream>
+#pragma once
+
+class Distance
+{
+private:
+    int feet_;
+    float inches_;
+
+public:
+    Distance() : feet_(0), inches_(0) {}
+    Distance(int feet, float inches) : feet_(feet), inches_(inches) {}
+    ~Distance(){}
+    // Getter & Setters
+    int feet() const {return feet_;}
+    void set_feet(int feet)  {feet_ = feet;}
+
+    float inches() const {return inches_;}
+    void set_inches(float inches)  {inches_ = inches;}
+    // Other Methods
+    void ShowDist() const;
+
+    Distance operator + (Distance rhs) const;
+    friend std::ostream &operator <<(std::ostream &os, const Distance& distance);
+
+    //Task 1
+    Distance operator - (Distance rhs) const;
+    //Task 2
+    void update_distnace(int ft, float in);
+};
